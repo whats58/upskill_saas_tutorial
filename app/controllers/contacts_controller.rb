6032b3f564d54ecb28c09f4e2a208f8ec53f039c
TJ Guzik <whats58@gmail.com>
2017-09-26 23:35:57 +0000

    Corrected typo in the contacts conroller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       name = params[:contact][:name]
-      email - params[:contact][:email]
+      email = params[:contact][:email]
       body = params[:contact][:comments]
       ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message sent."

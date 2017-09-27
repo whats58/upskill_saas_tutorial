4acdf087e303242fa85fcb45cab94b10b68e1ac6
TJ Guzik <whats58@gmail.com>
2017-09-27 00:08:34 +0000

    Removed duplicate message sent message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,7 +11,7 @@ class ContactsController < ApplicationController
       body = params[:contact][:comments]
       ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message sent."
-      redirect_to new_contact_path, notice: "Message sent."
+      redirect_to new_contact_path
     else
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
